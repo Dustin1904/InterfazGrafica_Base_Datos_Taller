5e3b84ebe4f290c11a54c1006e192b3635ddef1d
Dustin <dustin04ms@gmail.com>
2024-01-23 09:50:46 -0500

    segundo

--- EjercicioLogin/src/form2.java
+++ EjercicioLogin/src/form2.java
@@ -18,19 +18,28 @@ public class form2 extends form1{
         mostrarInformacion.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String dbURL="jdbc:mysql://localhost:3306/estudiantes";
+                String dbURL="jdbc:mysql://localhost:3306/correo_contraseña";
                 String dbuserName="root";
                 String dbpassword="Tsuki1904";
                 java.sql.Connection connection=null;
                 try{
                     Class.forName("com.mysql.cj.jdbc.Driver");
                     connection = DriverManager.getConnection(dbURL,dbuserName, dbpassword);
+                    Statement statement;
+                    statement=connection.createStatement();
+                    java.sql.ResultSet resultset;
+                    resultset=statement.executeQuery("select *from registro");
 
+                    while (resultset.next()){
+                        String nombre=resultset.getString("usuario");
+                        String contra=resultset.getString("contraseña");
+                        System.out.println(nombre + " ---" + contra);
+                    }
 
-
-                }catch (Exception ex){
-                    System.out.println(ex);
+                }catch (Exception exception){
+                    System.out.println(exception);
                 }
+
 
             }
         });
